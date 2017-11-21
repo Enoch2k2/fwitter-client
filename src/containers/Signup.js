d9d5f8a994b21e5da6e39d1428ab0403ee2b5405
Enoch2k2 <enoch2k2@hotmail.com>
2017-11-21 11:40:36 -0600

    created form and action handlers for signup page

--- src/containers/Signup.js
+++ src/containers/Signup.js
@@ -2,10 +2,37 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class Signup extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            email: '',
+            password: ''
+        }
+    }
+
+    handleChange = e => {
+        const { name, value } = e.target;
+        this.setState({[name]: value});
+    }
+
+    handleSubmit = e => {
+        e.preventDefault();
+        
+    }
+
     render(){
         return (
             <div>
-                Signup
+                <h1>Fwitter Signup</h1>
+                <hr />
+                <form onSubmit={this.handleSubmit}>
+                    <label htmlFor="email">Email: </label>
+                    <input type="email" name="email" value={this.state.email} onChange={e => this.handleChange(e)} id="email" />
+                    <label htmlFor="password">Password: </label>
+                    <input type="password" name="password" value={this.state.password} onChange={e => this.handleChange(e)} id="password" />
+                    <input type="submit" value="sign up" />
+                </form>
             </div>
         )
     }
